d94523b02c1ec4a0a46b5ef1c71a49a7d0744e73
drdna <farman@email.uky.edu>
2021-10-22 15:05:52 -0400

    Add files via upload

---
+++ FigS2/Cophylogeny.R
@@ -0,0 +1,26 @@
+# Description -------------------------------------------------------------
+
+## Takes two trees and rotates branches to generate best alignment
+
+## connects strain IDs with lines
+
+library(ape)
+library(phangorn)
+library(phytools)
+
+# Provide the objects
+
+Tree1 <- read.tree("ch7bac7noGaps.tre")
+
+Tree1
+
+Tree2 <- read.tree("MPG1noGaps.tre")
+
+cophylo <- cophylo(Tree1, Tree2, assoc=NULL, rotate=TRUE)
+
+pdf("cophylo4.pdf", 24, 44)
+
+plot(cophylo)
+
+dev.off()
+
