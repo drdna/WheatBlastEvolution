4ed5fd894bf1a61a26453aef5e385a80650d582e
Mark Farman <farman@email.uky.edu>
2023-04-17 12:28:48 -0400

    Update TMRREs.R

--- ARG/TMRREs.R
+++ ARG/TMRREs.R
@@ -3,9 +3,13 @@ library(gridExtra)
 
 setwd("/Users/mfarman")
 
-df <- read.table("TMRREsReal.txt", header = FALSE)
+df <- read.table("TMRREs.txt", header = FALSE)
 
 colnames(df) <- c('lineage', 'age')
 
 ggplot(df, aes(x=age)) + geom_bar(color = "blue", fill = "blue") + 
-  facet_grid(vars(lineage), scale = "free") + xlim(-1,1000)
+  facet_grid(vars(lineage), scale = "free") + xlim(-5,10000) +
+  ggtitle("Distribution of Time to Most Recent Recombination Event")+
+  xlab("years before present") +
+  ylab("# of interations") +
+  ylim(-1, 201)
