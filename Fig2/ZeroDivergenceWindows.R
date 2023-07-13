26df3a407c9e1ed33e61675c64d525ce4eecd96d
Mark Farman <farman@email.uky.edu>
2023-07-13 15:03:46 -0400

    Update ZeroDivergenceWindows.R

--- Fig2/ZeroDivergenceWindows.R
+++ Fig2/ZeroDivergenceWindows.R
@@ -3,20 +3,21 @@
 
 for (f in 1:7) {
   
-  print(paste0("Chr", f, sep = ""))
-  
   x <- as.data.frame(read.table(paste("~/NEE_SHINY/Chr", f, ".B71.diffs", sep = ""), header = T, row.names = 1))
 
   y <- x[x$clade != "L",]
   
   z <- y[y$clade != "T",]
-  
+
+  # determine minimum value in each column
   windows <- as.numeric(apply(z,1,min))
 
+  # extract columns with non-zero values
   nonzero <- windows[as.numeric(apply(z,1,min)) > 0]
 
+  # calculate proportion of columns with zero as lowest value
   proportionZero <- 1 - (length(nonzero)/length(windows))
 
-  print(paste0("Zero divergence windows = ", round(proportionZero, 2), "%", sep = ""))
+  print(paste0("Chr", f, ": windows exhibiting zero divergence = ", round(proportionZero, 2), "%", sep = ""))
 
 }
